0a34b1893b829fcfc94be8a224fdd54805ff4c9c
0942v8653 <0942v8653@users.noreply.github.com>
2014-11-03 18:57:25 -0500

    Created command-line tool

---
+++ sbar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sbar
+//
+//  Created by 0942v8653 on 11/3/14.
+//  Copyright (c) 2014 0942v8653. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
