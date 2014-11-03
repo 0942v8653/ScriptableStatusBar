ee8a12a42ad419185e89a39d582d22d7340d6508
0942v8653 <0942v8653@users.noreply.github.com>
2014-11-03 18:55:28 -0500

    Made into LSUIElement, deleted window

--- ScriptableStatusBar/AppDelegate.swift
+++ ScriptableStatusBar/AppDelegate.swift
@@ -1,18 +1,7 @@
-//
-//  AppDelegate.swift
-//  ScriptableStatusBar
-//
-//  Created by 0942v8653 on 11/3/14.
-//  Copyright (c) 2014 0942v8653. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
