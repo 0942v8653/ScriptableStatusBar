9c51e9cdcb048688e7a7b3d45c27ceb81a33124e
0942v8653 <0942v8653@users.noreply.github.com>
2014-11-03 18:51:40 -0500

    Initial Commit

---
+++ ScriptableStatusBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ScriptableStatusBar
+//
+//  Created by 0942v8653 on 11/3/14.
+//  Copyright (c) 2014 0942v8653. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScriptableStatusBarTests/ScriptableStatusBarTests.swift
@@ -0,0 +1,36 @@
+//
+//  ScriptableStatusBarTests.swift
+//  ScriptableStatusBarTests
+//
+//  Created by 0942v8653 on 11/3/14.
+//  Copyright (c) 2014 0942v8653. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class ScriptableStatusBarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
