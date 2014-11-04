39f87d593a79f93def5807de4a690c2193575bc8
0942v8653 <0942v8653@users.noreply.github.com>
2014-11-04 09:03:28 -0500

    It works

--- ScriptableStatusBar/AppDelegate.swift
+++ ScriptableStatusBar/AppDelegate.swift
@@ -2,15 +2,70 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    var statusBarItems = [String: NSStatusItem]()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        NSDistributedNotificationCenter.defaultCenter()
+            .addObserver(self, selector: Selector("recievedDistributedNotification:"), name: nil, object: nil)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
-
+    func recievedDistributedNotification(notification: NSNotification) {
+        if notification.name == "io.github.0942v8653.ScriptableStatusBar" {
+            println(notification)
+            let dict = notification.userInfo as Dictionary<String, AnyObject>
+            let identifier = dict["identifier"] as String
+            if dict["action"] as String == "remove" {
+                self.removeStatusBarItem(identifier)
+            } else {
+                self.setStatusBarItem(identifier,
+                    title: dict["title"] as String,
+                    menuItems: dict["menuItems"] as Dictionary<String, String>)
+            }
+        }
+    }
+    
+    func removeStatusBarItem(identifier: String) {
+        if let item = statusBarItems.removeValueForKey(identifier) {
+            NSStatusBar.systemStatusBar().removeStatusItem(item)
+        }
+    }
+    
+    func setStatusBarItem(identifier: String, title: String, menuItems: Dictionary<String, String>) {
+        let previousItem = statusBarItems[identifier]
+        var statusItem: NSStatusItem!
+        if previousItem != nil {
+            statusItem = previousItem!
+        } else {
+            // for some reason NSVariableStatusItemLength doesn't show up, so here's a quick hack
+            statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1.0)
+        }
+        statusItem.title = title
+        if menuItems.count > 0 {
+            println(menuItems)
+            let menu = NSMenu(title: title)
+            menu.autoenablesItems = false
+            for (menuItemTitle, shellScript) in menuItems {
+                let menuItem = NSMenuItem(title: menuItemTitle, action: Selector("menuItemClicked:"), keyEquivalent: "")
+                if shellScript == "__disabled__" {
+                    menuItem.enabled = false
+                } else {
+                    menuItem.representedObject = shellScript
+                }
+                menu.addItem(menuItem)
+            }
+            statusItem.menu = menu
+        }
+        statusBarItems[identifier] = statusItem
+    }
+    
+    func menuItemClicked(item: NSMenuItem) {
+        if let command = item.representedObject as? String {
+            NSTask.launchedTaskWithLaunchPath("/bin/bash", arguments: ["-c", command])
+        }
+    }
 }
 
--- sbar/main.swift
+++ sbar/main.swift
@@ -1,12 +1,60 @@
-//
-//  main.swift
-//  sbar
-//
-//  Created by 0942v8653 on 11/3/14.
-//  Copyright (c) 2014 0942v8653. All rights reserved.
-//
+import Cocoa
 
-import Foundation
+let notificationName = "io.github.0942v8653.ScriptableStatusBar"
+let arguments = Process.arguments[1 ..< Process.arguments.count]
 
-println("Hello, World!")
+func postNotification(dict: Dictionary<String, AnyObject>) {
+    NSDistributedNotificationCenter.defaultCenter()
+        .postNotificationName(notificationName, object: nil, userInfo: dict, deliverImmediately: true)
+}
 
+func main() -> Bool {
+    if arguments.count < 2 {
+        return printUsage()
+    }
+    if arguments[0] == "set" {
+        if arguments.count < 3 {
+            return printUsage()
+        }
+        let identifier = arguments[1]
+        let string = arguments[2]
+        var menuItems = [String: String]()
+        for i in arguments[3 ..< arguments.count] {
+            if let r = i.rangeOfString(":") {
+                menuItems[i.substringToIndex(r.startIndex)] = i.substringFromIndex(r.endIndex)
+            } else {
+                menuItems[i] = "__disabled__"
+            }
+        }
+        setStatusBarItem(identifier, string, menuItems)
+    } else if arguments[0] == "remove" {
+        removeStatusBarItem(arguments[1])
+    }
+    
+    return true
+}
+
+func setStatusBarItem(identifier: String, title: String, menuItems: Dictionary<String, String>) {
+    postNotification([
+        "action": "set",
+        "identifier": identifier,
+        "title": title,
+        "menuItems": menuItems
+    ])
+}
+
+func removeStatusBarItem(identifier: String) {
+    postNotification([
+        "action": "remove",
+        "identifier": identifier
+    ])
+}
+
+func printUsage() -> Bool {
+    println("Usage: ")
+    println("    sbar set <id> <string> [menuitem:command] [...]")
+    println("    sbar remove <id>")
+    return false
+}
+
+main()
